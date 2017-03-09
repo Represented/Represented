8fbb67a17208a9b9542f491216f4ae14836bb9c0
Tom Huibregtse <t.huibr@gmail.com>
2017-03-09 14:08:36 -0600

    db: initDB: skip validators for now

--- data/initDB.js
+++ data/initDB.js
@@ -13,34 +13,32 @@ MongoClient.connect(url, function(err, db) {
 	assert.equal(null, err);
 	console.log("Connected successfully to server");
 
-	createRepresentatives(db, function() {
-		db.close();
-	});
+	db.close();
 });
 
-// Create collections with document validation
-// http://mongodb.github.io/node-mongodb-native/2.2/tutorials/collections/
-// TODO check actionsID for type even though docs don't need any actions
-var createRepresentatives = function(db, callback) {
-  db.createCollection("representatives",
-	    {
-	      'validator': { '$and':
-	         [
-		    { 'name': { '$type': "string" } },
-		    { 'title': { '$type': "string" } }, // can be array
-		    { 'party': { '$type': "string" } },
-		    { 'biography': { '$type': "string" } },
-		    { 'portraitURL': { '$type': "string" } },
-		    { 'email': { '$type': "string" } },
-		    { 'phoneNumber': { '$type': "string" } },
-		    { 'location.state': { '$type': "string" } },
-		    { 'location.district': { '$type': "string" } },
-	         ]
-	      }
-	   },
-    function(err, results) {
-      console.log("Collection representatives created.");
-      callback();
-    }
-  );
-};
+//// Create collections with document validation
+//// http://mongodb.github.io/node-mongodb-native/2.2/tutorials/collections/
+//// TODO check actionsID for type even though docs don't need any actions
+//var createRepresentatives = function(db, callback) {
+//  db.createCollection("representatives",
+//	    {
+//	      'validator': { '$and':
+//	         [
+//		    { 'name': { '$type': "string" } },
+//		    { 'title': { '$type': "string" } }, // can be array
+//		    { 'party': { '$type': "string" } },
+//		    { 'biography': { '$type': "string" } },
+//		    { 'portraitURL': { '$type': "string" } },
+//		    { 'email': { '$type': "string" } },
+//		    { 'phoneNumber': { '$type': "string" } },
+//		    { 'location.state': { '$type': "string" } },
+//		    { 'location.district': { '$type': "string" } },
+//	         ]
+//	      }
+//	   },
+//    function(err, results) {
+//      console.log("Collection representatives created.");
+//      callback();
+//    }
+//  );
+//};

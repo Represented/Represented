18cd4f564e1a81b5ab0d967bd0d3e105fa145491
Tom Huibregtse <t.huibr@gmail.com>
2017-03-13 10:47:04 -0500

    db: unitTests: add findGoodDistrict test

--- data/unitTests.js
+++ data/unitTests.js
@@ -2,6 +2,8 @@ var mutateDB = require('./mutateDB');
     MongoClient = require('mongodb').MongoClient,
     assert = require('assert');
 
+var url = 'mongodb://localhost:27017/myproject';
+
 exports.validateBadDistrict = function(test) {
 	test.ifError(mutateDB.validateDistrict(
 		{
@@ -75,76 +77,103 @@ exports.validateGoodDistrict2 = function(test) {
 	test.done();
 };
 
-
-var url = 'mongodb://localhost:27017/unit_testing_db';
-
 exports.insertDistrictWithNoArgs = function(test) {
 	test.ifError(mutateDB.insertDistrict());
 	test.done();
 };
 
-var url = 'mongodb://localhost:27017/myproject';
-
 exports.insertDistrictWithMissingArgs = function(test) {
 	MongoClient.connect(url, function(err, db) {
 		assert.equal(null, err);
 
+		// second param never even declared
 		test.ifError(mutateDB.insertDistrict(db));
 		db.close();
 		db.dropDatabase();
+		test.expect(1);
+		test.done();
 	});
-	test.done();
 };
 
-exports.insertDistrictWithMissingArgs = function(test) {
-	MongoClient.connect(url, function(err, db) {
-		assert.equal(null, err);
-
-		test.ifError(mutateDB.insertDistrict(db));
-		db.close();
-		db.dropDatabase();
-	});
-	test.done();
-};
-
-exports.insertDistrictWithMissingArgs = function(test) {
-	MongoClient.connect(url, function(err, db, test) {
-		assert.equal(null, err);
-
-		test.ok(mutateDB.insertDistrict(db));
-	});
-
-	test.expect(1);
-	test.done();
-};
 
 exports.insertBadDistrict = function(test) {
 	MongoClient.connect(url, function(err, db) {
 		assert.equal(null, err);
-		test.ok(mutateDB.insertDistrict(db,
+		test.ifError(mutateDB.insertDistrict(db,
 			{
 				state: "WI",
 			}
 		));
 		db.dropDatabase();
-	});
-
 	test.expect(1);
 	test.done();
+	});
 };
 
-//exports.insertDistrict = function(test) {
+exports.insertGoodDistrict = function(test) {
+	MongoClient.connect(url, function(err, db) {
+		assert.equal(null, err);
+		test.ok(mutateDB.insertDistrict(db,
+			{
+				state: "WI",
+				district: 1,
+			}, function(result) {
+			db.dropDatabase();
+			test.ok(true);
+			test.expect(2);
+			test.done();
+		}));
+	});
+};
+
+exports.findDistrictWithMissingArgs = function(test) {
+	MongoClient.connect(url, function(err, db) {
+		assert.equal(null, err);
+
+		// second and third param never even declared
+		test.ifError(mutateDB.findDistrict(db));
+		db.close();
+		db.dropDatabase();
+		test.expect(1);
+		test.done();
+	});
+};
+
+//exports.findBadDistrict = function(test) {
 //	MongoClient.connect(url, function(err, db) {
 //		assert.equal(null, err);
-//		test.ok(mutateDB.insertDistrict(db,
+//		test.ifError(mutateDB.findDistrict(db,
 //			{
 //				state: "WI",
-//				district: 1,
-//				zipcodes: [53527,],
 //			}
 //		));
-//		db.close();
 //		db.dropDatabase();
+//		test.expect(1);
+//		test.done();
 //	});
-//	test.done();
 //};
+
+exports.findGoodDistrict = function(test) {
+	MongoClient.connect(url, function(err, db) {
+		assert.equal(null, err);
+		var dist = {
+				state: "WI",
+				district: 1,
+			   };
+		test.ok(mutateDB.insertDistrict(db, dist, function(result) {
+			// _id gets tacked on the end of dist; we need to put it at the front to
+			// test equality because findDistrcit() has _id as first field
+			dist = {
+					_id: dist._id,
+					state: dist.state,
+					district: dist.district,
+				};
+			test.ok(mutateDB.findDistrict(db, dist, function(docs) {
+				test.ok(JSON.stringify(dist) == JSON.stringify(docs[0]), JSON.stringify(dist)+" doesn't match returned"+JSON.stringify(docs[0]));
+				db.dropDatabase();
+				test.expect(3);
+				test.done();
+			}));
+		}));
+	});
+};

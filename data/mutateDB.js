5cb72b5fcc02138b30187bb7622f26a0dfbd14f1
Tom Huibregtse <t.huibr@gmail.com>
2017-03-13 11:01:23 -0500

    db: mutateDB: remove unused delete db function

--- data/mutateDB.js
+++ data/mutateDB.js
@@ -70,34 +70,3 @@ var findDistrict = function(db, district, callback) {
 	return true;
 };
 module.exports.findDistrict = findDistrict;
-
-
-// http://mongodb.github.io/node-mongodb-native/2.2/api/Db.html#dropDatabase
-var removeDB = function(db, callback) {
-	db.dropDatabase(function(err, result) {
-		assert.equal(null, err);
-
-		// Wait two seconds to let it replicate across
-		setTimeout(function() {
-			// Get the admin database
-			db.admin().listDatabases(function(err, dbs) {
-				// Grab the databases
-				dbs = dbs.databases;
-				// Did we find the db
-				var found = false;
-
-				// Check if we have the db in the list
-				for(var i = 0; i < dbs.length; i++) {
-					if(dbs[i].name == 'integration_tests_to_drop') found = true;
-				}
-
-				// We should not find the databases
-				if(process.env['JENKINS'] == null) test.equal(false, found);
-
-				db.close();
-			});
-			console.log("Dropped the database");
-		}, 2000);
-	});
-}
-module.exports.removeDB = removeDB;

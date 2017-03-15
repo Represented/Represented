d726040b5879223b31177cf5b4b0ee0ad2433bb7
zaydman <bzaydman@wisc.edu>
2017-03-15 16:03:15 -0500

    setup of primeng complete

--- WebApp/web-app/src/systemjs.config.js
+++ WebApp/web-app/src/systemjs.config.js
@@ -26,7 +26,8 @@
 
       // other libraries
       'rxjs':                      'npm:rxjs',
-      'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js'
+      'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js',
+      'primeng':                   'node_modules/primeng'
     },
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
@@ -36,6 +37,9 @@
       },
       rxjs: {
         defaultExtension: 'js'
+      },
+      primeng: { 
+        defaultExtension: 'js' 
       }
     }
   });

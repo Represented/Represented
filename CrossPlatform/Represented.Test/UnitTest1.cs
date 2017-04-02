57557f5e10d37c76b91742d545fc8529bc25ff65
jsimonson <jsimonson2013@gmail.com>
2017-04-02 16:01:39 -0500

    Added testing project.

---
+++ CrossPlatform/Represented.Test/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace Represented.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        List<Represented.Model.RepresentedItem> db_DOC;
+        Represented.Model.RepresentedItem repItem = new Represented.Model.RepresentedItem();
+
+        public Represented.App setUp()
+        {
+            db_DOC = new List<Represented.Model.RepresentedItem>();
+            return new Represented.App();
+        }
+
+        public void tearDown()
+        {
+            db_DOC.Clear();
+        }
+
+        [TestMethod]
+        public void TestPutZip(string zip)
+        {
+            var app = setUp();
+            app.checkStoredLocation();
+            tearDown();
+        }
+    }
+}

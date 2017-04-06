39fa203975a2ea0340d1bcae655452c6d6899e3c
Gregory Spicer <gdspicer58@gmail.com>
2017-04-06 13:43:22 -0500

    put newsfeed component back in since it apparently was deleted in my last commit

---
+++ src/app/newsfeed.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Router }            from '@angular/router';
+import { RepAction }                from './repaction';
+import { RepActionService }         from './repaction.service';
+import { DataScrollerModule }       from 'primeng/primeng';
+import { CookieService } 			from 'angular2-cookie/services/cookies.service';
+import { Jsonp } 					from '@angular/http';
+
+@Component({
+  moduleId: module.id,
+  providers: [CookieService],
+  selector: 'my-newsfeed',
+  templateUrl: '../views/newsfeed.component.html',
+  styleUrls: [ '../styles/newsfeed.component.css' ]
+})
+export class NewsfeedComponent implements OnInit {
+  repactions: RepAction[];
+
+  constructor(
+    private repActionService: RepActionService,
+    private router: Router,
+	private cookieService: CookieService,
+	private jsonp: Jsonp) { }
+
+  /*getActions(): void {
+    this.repActionService
+        .getActions()
+        .then(repactions => this.repactions = repactions);
+  }*/
+  ngOnInit(): void {
+	var representatives = this.cookieService.getObject('bioguides');
+	if(representatives === undefined){
+		this.router.navigate(['/welcome']);
+	}
+  }
+}
